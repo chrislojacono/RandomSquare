a3119c87a4acdff9c91aa9766ed47bcfcf584f3a
Chris LoJacono <clojacono92@gmail.com>
2021-02-02 11:19:05 -0600

    finished

--- RandomSquare/Program.cs
+++ RandomSquare/Program.cs
@@ -12,12 +12,30 @@ namespace RandomSquare
 
             for (int ctr = 0; ctr <= 19; ctr++)
                 assortedList.Add(random.Next(1, 50));
-
             foreach (var item in assortedList)
             {
                 Console.WriteLine($"{item}");
             }
-            
+
+            var finalList = new List<int>();
+            foreach (var item in assortedList)
+            {
+                var squaredValue = item * item;
+                finalList.Add(squaredValue);
+            }
+            foreach (var item in finalList)
+            {
+                Console.WriteLine($"{item}");
+            }
+            //Removes all odd integers. The for each wont work because you are modifiying the list as you go through it 
+
+            finalList.RemoveAll((i) => i % 2 != 0);
+
+            foreach (var item in finalList)
+            {
+                Console.WriteLine($"{item}");
+            }
+
         }
     }
 }
