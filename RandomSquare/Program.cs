548d4890cdb87872d773c5797eac30c0776d47ce
Chris LoJacono <clojacono92@gmail.com>
2021-02-02 11:09:04 -0600

    random numbers generated

---
+++ RandomSquare/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var assortedList = new List<int>();
+
+            for (int ctr = 0; ctr <= 19; ctr++)
+                assortedList.Add(random.Next(1, 50));
+
+            foreach (var item in assortedList)
+            {
+                Console.WriteLine($"{item}");
+            }
+            
+        }
+    }
+}
